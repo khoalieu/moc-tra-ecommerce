7bc6f21b4dab2ea045b9147d7cb2e15b169bd97d
HieuHiu0512 <23130108@st.hcmuaf.edu.vn>
2026-05-22 22:37:51 +0700

    feat: [Tính năng] Thống kê Sản phẩm & Người dùng (Dashboard Admin)

---
+++ src/main/java/model/product/TopProductDTO.java
@@ -0,0 +1,22 @@
+package model.product;
+
+public class TopProductDTO {
+    private String productName;
+    private int totalSold;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getTotalSold() {
+        return totalSold;
+    }
+
+    public void setTotalSold(int totalSold) {
+        this.totalSold = totalSold;
+    }
+}
