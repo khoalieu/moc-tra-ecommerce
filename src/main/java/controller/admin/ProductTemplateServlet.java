6768d91b8d6c32b816d48b0639c9f5a464a01a3e
HieuHiu0512 <23130108@st.hcmuaf.edu.vn>
2026-05-23 23:10:47 +0700

    fix: chỉnh sửa template

--- src/main/java/controller/admin/ProductTemplateServlet.java
+++ src/main/java/controller/admin/ProductTemplateServlet.java
@@ -17,26 +17,29 @@ public class ProductTemplateServlet extends HttpServlet {
         response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
         response.setHeader("Content-Disposition", "attachment; filename=product_import_template.xlsx");
         Workbook workbook = new XSSFWorkbook();
+        CellStyle headerStyle = workbook.createCellStyle();
+        Font font = workbook.createFont();
+        font.setBold(true);
+        headerStyle.setFont(font);
         Sheet sheet = workbook.createSheet("Products");
-
         Row header = sheet.createRow(0);
-
         String[] columns = {
-                "Name",
-                "Price",
-                "SalePrice",
-                "Stock",
-                "Category",
-                "Status",
-                "IsBestseller",
-                "ShortDescription",
-                "Description"
+                "Tên sản phẩm",
+                "Giá gốc",
+                "Giá sale",
+                "Số lượng",
+                "Danh mục",
+                "Trạng thái",
+                "Bán chạy",
+                "Mô tả ngắn",
+                "Mô tả chi tiết",
+                "Tệp hình ảnh"
         };
 
         for (int i = 0; i < columns.length; i++) {
             Cell cell = header.createCell(i);
             cell.setCellValue(columns[i]);
-            sheet.autoSizeColumn(i);
+            cell.setCellStyle(headerStyle);
         }
 
         Row sample = sheet.createRow(1);
@@ -46,9 +49,14 @@ public class ProductTemplateServlet extends HttpServlet {
         sample.createCell(3).setCellValue(50);
         sample.createCell(4).setCellValue("Trà trái cây");
         sample.createCell(5).setCellValue("ACTIVE");
-        sample.createCell(6).setCellValue("false");
-        sample.createCell(7).setCellValue("Mô tả ngắn sản phẩm");
-        sample.createCell(8).setCellValue("Mô tả chi tiết sản phẩm");
+        sample.createCell(6).setCellValue("true");
+        sample.createCell(7).setCellValue("Trà đào thanh mát");
+        sample.createCell(8).setCellValue("Trà đào cam sả phù hợp uống lạnh");
+        sample.createCell(9).setCellValue("tra-dao-cam-sa.png");
+
+        for (int i = 0; i < columns.length; i++) {
+            sheet.autoSizeColumn(i);
+        }
 
         workbook.write(response.getOutputStream());
         workbook.close();
